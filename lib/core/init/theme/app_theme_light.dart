3247dea0386f3a955117cbe9c2e60b90cbb1eef9
akaanuzman <uzmanakan@gmail.com>
2021-05-24 15:05:12 +0300

    theme

--- lib/core/init/theme/app_theme_light.dart
+++ lib/core/init/theme/app_theme_light.dart
@@ -1,8 +1,10 @@
+import 'package:application_architecture/core/init/theme/light/light_theme_interface.dart';
+import 'package:application_architecture/core/init/theme/light/text_theme_light.dart';
 import 'package:flutter/material.dart';
 
 import 'app_theme.dart';
 
-class AppThemeLight extends AppTheme{
+class AppThemeLight extends AppTheme with ILightTheme {
   static AppThemeLight? _instance;
   static AppThemeLight get instance {
     if (_instance != null)
@@ -15,5 +17,30 @@ class AppThemeLight extends AppTheme{
 
   AppThemeLight._init();
 
-  ThemeData get theme => ThemeData.light();
+  ThemeData get theme => ThemeData.light()
+      .copyWith(colorScheme: _colorScheme, textTheme: _textTheme);
+
+  ColorScheme get _colorScheme => ColorScheme(
+      primary: colorSchemeLight.black,
+      primaryVariant: colorSchemeLight.azure,
+      secondary: Colors.green,
+      secondaryVariant: Colors.indigo,
+      surface: Colors.blue, //xx
+      background: Color(0xfff6f9fc), //xx
+      error: Colors.red[900]!,
+      onPrimary: Colors.greenAccent,
+      onSecondary: Colors.black, //x
+      onSurface: Colors.white30,
+      onBackground: Colors.black12,
+      onError: Color(0xFFF9B916), //xx
+      brightness: Brightness.light);
+
+  TextTheme get _textTheme => TextTheme(
+        headline1: textThemeLight.headline1,
+        headline2: textThemeLight.headline2,
+        headline3: textThemeLight.headline3,
+        headline4: textThemeLight.headline4,
+        headline5: textThemeLight.headline5,
+        headline6: textThemeLight.headline6,
+      );
 }
---
+++ lib/core/init/theme/light/text_theme_light.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class TextThemeLight {
+  static TextThemeLight? _instance;
+  static TextThemeLight get instance {
+    if (_instance != null)
+      return _instance!;
+    else {
+      _instance = TextThemeLight.init();
+      return _instance!;
+    }
+  }
+
+  TextThemeLight.init();
+
+  final TextStyle headline1 =
+      TextStyle(fontSize: 96, fontWeight: FontWeight.w300, letterSpacing: -1.5);
+  final TextStyle headline2 =
+      TextStyle(fontSize: 60, fontWeight: FontWeight.w300, letterSpacing: -0.5);
+  final TextStyle headline3 =
+      TextStyle(fontSize: 48, fontWeight: FontWeight.w400);
+  final TextStyle headline4 =
+      TextStyle(fontSize: 34, fontWeight: FontWeight.w400, letterSpacing: 0.25);
+  final TextStyle headline5 =
+      TextStyle(fontSize: 24, fontWeight: FontWeight.w400);
+  final TextStyle headline6 =
+      TextStyle(fontSize: 20, fontWeight: FontWeight.w500, letterSpacing: 0.15);
+
+  final TextStyle overline =
+      TextStyle(fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5);
+}
